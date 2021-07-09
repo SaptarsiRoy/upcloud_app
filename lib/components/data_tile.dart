fc11757a246d5e27b6a42e7ceb1e814cb177b6a9
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-10 01:47:48 +0530

    added padding to column widget

--- lib/components/data_tile.dart
+++ lib/components/data_tile.dart
@@ -1,45 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:upcloud_app/constants.dart';
 
 class DataTile extends StatelessWidget {
-  final String name, email, contact, adress;
+  final String name, email, contact, address;
 
   DataTile({
     required this.name,
     required this.email,
     required this.contact,
-    required this.adress,
+    required this.address,
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: 300,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Name'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Email'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Contact'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Address'),
-          ),
-        ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 40.0,
       ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(
-          10.0,
+      child: Container(
+        height: 250,
+        width: 300,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: 30.0,
+                left: 20.0,
+              ),
+              child: Text(
+                name,
+                style: kDataTileTextStyle,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 30.0,
+                left: 20.0,
+              ),
+              child: Text(
+                email,
+                style: kDataTileTextStyle,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 30.0,
+                left: 20.0,
+              ),
+              child: Text(
+                contact,
+                style: kDataTileTextStyle,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 30.0,
+                left: 20.0,
+              ),
+              child: Text(
+                address,
+                style: kDataTileTextStyle,
+              ),
+            ),
+          ],
         ),
-        color: Colors.grey,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            10.0,
+          ),
+          color: Colors.grey.shade300,
+        ),
       ),
     );
   }
