8c8c4c368260ef151017bbd233980e853022ab6d
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-09 20:56:00 +0530

    changed to listview

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -7,83 +7,103 @@ class AddData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  DataButton(
-                    textTitle: 'Add Data',
-                    onPressed: () {},
-                    backgroundColor: Color(0xFFFFC19E),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DataButton(
+                        textTitle: 'Add Data',
+                        onPressed: () {},
+                        backgroundColor: Color(0xFFFFC19E),
+                      ),
+                      DataButton(
+                        textTitle: 'Show Data',
+                        onPressed: () {},
+                        backgroundColor: Colors.grey.shade300,
+                      ),
+                    ],
                   ),
-                  DataButton(
-                    textTitle: 'Show Data',
+                  SizedBox(
+                    height: 100.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Name:",
+                        //labelStyle: TextStyle()
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "E-mail:",
+                        //labelStyle: TextStyle()
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Contact:",
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Address:",
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xFFFFC19E),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
                     onPressed: () {},
-                    backgroundColor: Colors.grey.shade300,
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 50.0,
+                          vertical: 10.0,
+                        ),
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0,
+                          ),
+                        )),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 100.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Name:",
-                    //labelStyle: TextStyle()
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "E-mail:",
-                    //labelStyle: TextStyle()
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Contact:",
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Address:",
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              DataButton(
-                textTitle: 'Save',
-                onPressed: () {},
-                backgroundColor: Color(0xFFFFC19E),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
