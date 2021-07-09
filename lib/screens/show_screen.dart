b611949a77140cd80242cba7dd462ce35b095946
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-09 21:32:22 +0530

    added buttons

--- lib/screens/show_screen.dart
+++ lib/screens/show_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:upcloud_app/components/data_button.dart';
 
 class ShowData extends StatefulWidget {
   static const String id = 'ShowData';
@@ -9,6 +10,33 @@ class ShowData extends StatefulWidget {
 class _ShowDataState extends State<ShowData> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DataButton(
+                  textTitle: 'Add Data',
+                  onPressed: () {},
+                  backgroundColor: Color(0xFFFFC19E),
+                ),
+                DataButton(
+                  textTitle: 'Show Data',
+                  onPressed: () {
+                    Navigator.pushNamed(context, ShowData.id);
+                  },
+                  backgroundColor: Colors.grey.shade300,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
