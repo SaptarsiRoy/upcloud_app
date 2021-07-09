68da3bcb46bf59e0a5fa09ed6c02afc2dd28c5de
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-09 21:23:52 +0530

    added show screen

---
+++ lib/screens/show_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShowData extends StatefulWidget {
+  static const String id = 'ShowData';
+  @override
+  _ShowDataState createState() => _ShowDataState();
+}
+
+class _ShowDataState extends State<ShowData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
